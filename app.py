77ca06badee0ff54a4c4b3ec7f7f67af2e5d4ee5
Gregory Lindsey <gclindsey@gmail.com>
2024-03-23 15:29:05 -0700

    clean up

--- app.py
+++ app.py
@@ -8,13 +8,9 @@ from src.chat import respond_cot, ConversationContext, UserInteraction
 
 st.title("DSPy Chatbot")
 
-# # set default model
-# if "openai_model" not in st.session_state:
-#     st.session_state["openai_model"] = "gpt-3.5-turbo"
-
 # initialize conversation context
 if "context" not in st.session_state:
-    st.session_state["context"] = ConversationContext(window_size=5)
+    st.session_state["context"] = ConversationContext(window_size=10)
 
 # initialize chat history
 if "messages" not in st.session_state:
@@ -36,25 +32,14 @@ if prompt := st.chat_input("Enter message..."):
     # add user to chat history
     st.session_state.messages.append(st.session_state["interaction"].serialize_by_role()[0])
 
+    # display user input in chat container
     with st.chat_message("user"):
         st.markdown(st.session_state["interaction"].message)
 
     # display assistant responses in chat container
     with st.chat_message("assistant"):
-        # stream = client.chat.completions.create(
-        #     model=st.session_state["openai_model"],
-        #     messages=[
-        #         {
-        #             "role": m["role"],
-        #             "content": m["content"]
-        #         } for m in st.session_state.messages
-        #     ],
-        #     stream=True
-        # )
 
-        # response = st.write_stream(stream)
-
-
+        # get assistant response
         st.session_state["interaction"].response = respond_cot(
             context=st.session_state["context"].render(),
             message=st.session_state["interaction"].message
@@ -62,6 +47,7 @@ if prompt := st.chat_input("Enter message..."):
         
         st.markdown(st.session_state["interaction"].response)
 
+    # add assistant to chat history
     st.session_state.messages.append(st.session_state["interaction"].serialize_by_role()[1])
 
     # update conversation context
